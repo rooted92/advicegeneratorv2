560fb260badb1f507cb00c751b523cc2109bad7c
Pedro Castaneda <petecast76@gmail.com>
2023-03-15 08:33:32 -0700

    Fixed dividers not alternating

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -10,7 +10,7 @@ const Display = () => {
     const [slipID, setSlipID] = UseAdviceState("");
 
     // will use useState to check if screensize if less than or equal to 375px
-    const [isMobile, setIsMobile] = useState(false);
+    // const [isMobile, setIsMobile] = useState(false);
 
     // saving svg's to variables
     const mobileDivider = (<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" /><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3" /><rect x="14" width="6" height="16" rx="3" /></g></g></svg>);
@@ -31,10 +31,10 @@ const Display = () => {
         GetAdviceData()
     }, []);
 
-    useEffect(() => {
-        setIsMobile(window.innerWidth <= 375);// upon rendering useEffect will check if screen size is less than 375px if it is then will it show the mobile divider and if not it will show the desktop divider
-        console.log(isMobile);
-    }, [isMobile]);
+    // useEffect(() => {
+        // setIsMobile(window.innerWidth <= 375);// upon rendering useEffect will check if screen size is less than 375px if it is then will it show the mobile divider and if not it will show the desktop divider
+    //     console.log(isMobile);
+    // }, [isMobile]);
 
     return (
         <div className='d-flex justify-content-center'>
@@ -45,7 +45,12 @@ const Display = () => {
                         <p className='adviceText px-5 mb-5'>&ldquo;{advice}&rdquo;</p>
                         <div className='divider'>
                             {/* Ternary to check if isMobile is true or false. If true display mobile divider else display desktop divider */}
-                            {isMobile ? mobileDivider : desktopDivider}
+                            {/* {isMobile ? mobileDivider : desktopDivider} */}
+                            <picture>
+                                {/* when screen size is greater than 375px desktop divider will show */}
+                                <source media="(min-width: 375px)" srcSet={desktopDivider}></source>
+                                <img src={mobileDivider} alt='divider'></img> 
+                            </picture>
                         </div>
                         <DiceButton callFunc={GetAdviceData} />
                         {/* Remember to pass in a prop for the function you want to call in the Button component to be able to use onClick */}
