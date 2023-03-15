b98e295d29df4e80dc17caf0d07cd0231aa367b0
Pedro Castaneda <petecast76@gmail.com>
2023-03-15 08:59:48 -0700

    Fixed bug with source tag in picture element

--- src/components/display/Display.js
+++ src/components/display/Display.js
@@ -2,20 +2,15 @@ import React, { useEffect, useState } from 'react';
 import DiceButton from "../button/Button";
 import { Container, Row, Col } from 'react-bootstrap';
 import UseAdviceState from '../../hooks/AdviceStateHook';
+import DesktopDivider from '../../images/pattern-divider-desktop.svg';
+import MobileDivider from '../../images/pattern-divider-mobile.svg';
 import './Display.css';
 
 const Display = () => {
+
     // using custom hook from AdviceStateHook.js (it's the same exact thing as useState just wanted to mess around with custom hooks)
     const [advice, setAdvice] = UseAdviceState("");
     const [slipID, setSlipID] = UseAdviceState("");
-
-    // will use useState to check if screensize if less than or equal to 375px
-    // const [isMobile, setIsMobile] = useState(false);
-
-    // saving svg's to variables
-    const mobileDivider = (<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" /><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3" /><rect x="14" width="6" height="16" rx="3" /></g></g></svg>);
-
-    const desktopDivider = (<svg width="295" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="none" fill-rule="evenodd"><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" /><g transform="translate(138)" fill="#CEE3E9"><rect width="6" height="16" rx="3" /><rect x="14" width="6" height="16" rx="3" /></g></g></svg>);
 
     // Using async function to get data from advice api
     const GetAdviceData = async () => {
@@ -26,15 +21,11 @@ const Display = () => {
         setSlipID(data.slip.id);// saving id data from api to id variable
         console.log('Ive been clicked!');// test
     }
+
     // useEffect will invoke GetAdviceData and display data upon render
     useEffect(() => {
         GetAdviceData()
     }, []);
-
-    // useEffect(() => {
-        // setIsMobile(window.innerWidth <= 375);// upon rendering useEffect will check if screen size is less than 375px if it is then will it show the mobile divider and if not it will show the desktop divider
-    //     console.log(isMobile);
-    // }, [isMobile]);
 
     return (
         <div className='d-flex justify-content-center'>
@@ -44,14 +35,13 @@ const Display = () => {
                         <p className='slipIdText mt-5'>ADVICE #{slipID}</p>
                         <p className='adviceText px-5 mb-5'>&ldquo;{advice}&rdquo;</p>
                         <div className='divider'>
-                            {/* Ternary to check if isMobile is true or false. If true display mobile divider else display desktop divider */}
-                            {/* {isMobile ? mobileDivider : desktopDivider} */}
                             <picture>
-                                {/* when screen size is greater than 375px desktop divider will show */}
-                                <source media="(min-width: 375px)" srcSet={desktopDivider}></source>
-                                <img src={mobileDivider} alt='divider'></img> 
+                                {/* when screen size is less than 550px mobile divider will show */}
+                                <source media="(max-width: 550px)" srcSet={MobileDivider}></source>
+                                <img src={DesktopDivider} alt='divider'></img> 
                             </picture>
                         </div>
+                                    {/* vvv */}
                         <DiceButton callFunc={GetAdviceData} />
                         {/* Remember to pass in a prop for the function you want to call in the Button component to be able to use onClick */}
                     </Col>
